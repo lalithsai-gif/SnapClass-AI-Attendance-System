aedfa6e79c93a508283fedc3c55f4d74a836dfcf
lalithsai <lalithsaidabbiru@gmail.com>
2026-06-04 09:21:42 +0530

    streamlit setup+ g

---
+++ src/components/footer.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def footer_home():
+    st.markdown(f"""
+            <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; gap:6px;">
+                <p style="font-weight:bold; color:yellow;">Created by Lalith with ❤️</p>
+            </div>
+    """
+                ,unsafe_allow_html=True)
